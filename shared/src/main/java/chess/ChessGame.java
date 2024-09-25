76ceec026a316757264b8e6a7808437b4ae44766
samuelSchulthies <samuelschulthies@gmail.com>
2024-09-24 22:42:49 -0600

    Got 'white in checkmate' and 'black in checkmate by pawns' working. Scuffed code, hate it. Rework

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -1,8 +1,6 @@
 package chess;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Objects;
+import java.util.*;
 
 /**
  * For a class that can manage a chess game, making moves on a board
@@ -12,9 +10,9 @@ import java.util.Objects;
  */
 public class ChessGame {
 
-    ChessBoard gameBoard;
-    TeamColor teamTurn;
-    ChessPosition kingLocation;
+    static ChessBoard gameBoard;
+    static TeamColor teamTurn;
+    static ChessPosition kingLocation;
 
     public ChessGame() {
 
@@ -121,8 +119,96 @@ public class ChessGame {
      * @return True if the specified team is in checkmate
      */
     public boolean isInCheckmate(TeamColor teamColor) {
-        //TODO: Checkmate logic Next
-        throw new RuntimeException("Not implemented");
+        int row;
+        int col;
+
+        Collection<ChessMove> validKingMovesCollection = new ArrayList<>();
+        Collection<ChessMove> validMovesCollection = new ArrayList<>();
+        HashSet<ChessPosition> validKingSet = new HashSet<>();
+
+
+        for (row = 1; row < 9; ++row) {
+            for (col = 1; col < 9; ++col) {
+                ChessPosition positionChecker = new ChessPosition(row, col);
+
+                if (gameBoard.getPiece(positionChecker) != null) {
+                    if ((gameBoard.getPiece(positionChecker).getPieceType() == ChessPiece.PieceType.KING) &&
+                            (gameBoard.getPiece(positionChecker).getTeamColor() == teamColor)) {
+                        validKingMovesCollection = validMoves(positionChecker);
+                    }
+                }
+            }
+        }
+
+        //Checks for pawn controlled squares
+        for (ChessMove kingMove : validKingMovesCollection) {
+            int signRow = 1;
+            int signCol = 1;
+            for (int i = 0; i < 4; ++i) {
+
+                // i = 0 is ++ quadrant
+                // i = 1 is -+ quadrant
+                // i = 2 is -- quadrant
+                // i = 3 is +- quadrant
+
+                if (i == 1) {
+                    signRow = -1;
+                }
+                if (i == 2) {
+                    signRow = -1;
+                    signCol = -1;
+                }
+                if (i == 3) {
+                    signRow = 1;
+                    signCol = -1;
+                }
+
+                int rowToAdd = kingMove.getEndPosition().getRow() + (signRow);
+                int colToAdd = kingMove.getEndPosition().getColumn() + (signCol);
+                ChessPosition pawnPositionChecker = new ChessPosition(rowToAdd, colToAdd);
+
+                if ((rowToAdd > 0) && (rowToAdd < 9)
+                        && (colToAdd > 0) && (colToAdd < 9)) {
+                    if ((gameBoard.getPiece(pawnPositionChecker) != null)
+                            && (gameBoard.getPiece(pawnPositionChecker).getPieceType() == ChessPiece.PieceType.PAWN)) {
+                        ChessMove pawnControlled = new ChessMove(pawnPositionChecker, kingMove.getEndPosition(), null);
+                        validKingSet.add(pawnControlled.getEndPosition());
+                    }
+                }
+            }
+        }
+
+        int kingMovesSize = validKingMovesCollection.size();
+
+        for (row = 1; row < 9; ++row) {
+            for (col = 1; col < 9; ++col) {
+                ChessPosition positionChecker = new ChessPosition(row, col);
+
+                //TODO: Need logic that retrieves all squares controlled by a piece, not just valid moves
+                //TODO: Maybe retrieve valid moves again based on squares a piece controls
+                //TODO: if king moves and a new valid move comes up (pawn), then add that move to attacks against the king
+                //TODO: Make every move from valid moves and check if that move has put the king into check or checkmate
+                //TODO: All we need to do is call valid moves on every valid move from a piece and then run
+                // check/checkmate to see if our king has been put in danger. If it returns true we discard those moves
+                //TODO: Move the king to each of its valid moves and then check at each of those moves if it has a new
+                // attack (likely from pawn). Add that to list of possible attacks against the king.
+
+                if (gameBoard.getPiece(positionChecker) != null) {
+                    if (gameBoard.getPiece(positionChecker).getTeamColor() != teamColor) {
+                        validMovesCollection = validMoves(positionChecker);
+                        for (ChessMove move : validMovesCollection) {
+                            for (ChessMove kingMove : validKingMovesCollection) {
+                                if ((move.getEndPosition().getRow() == kingMove.getEndPosition().getRow()) &&
+                                        (move.getEndPosition().getColumn() == kingMove.getEndPosition().getColumn())){
+                                    validKingSet.add(move.getEndPosition());
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return validKingSet.size() == kingMovesSize;
     }
 
     /**
@@ -158,17 +244,12 @@ public class ChessGame {
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-
         ChessGame chessGame = (ChessGame) o;
-
-        if (!Objects.equals(gameBoard, chessGame.gameBoard)) return false;
-        return teamTurn == chessGame.teamTurn;
+        return Objects.equals(gameBoard, chessGame.gameBoard) && teamTurn == chessGame.teamTurn;
     }
 
     @Override
     public int hashCode() {
-        int result = gameBoard != null ? gameBoard.hashCode() : 0;
-        result = 31 * result + (teamTurn != null ? teamTurn.hashCode() : 0);
-        return result;
+        return Objects.hash(gameBoard, teamTurn);
     }
 }
