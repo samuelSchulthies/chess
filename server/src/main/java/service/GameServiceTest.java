f016220bd9f816f63f0960f830d2263eee1ef0ec
samuelSchulthies <samuelschulthies@gmail.com>
2024-10-21 19:51:54 -0600

    working negatve join result

--- server/src/main/java/service/GameServiceTest.java
+++ server/src/main/java/service/GameServiceTest.java
@@ -78,7 +78,43 @@ public class GameServiceTest {
     @Test
     @DisplayName("Negative Join Result")
     public void negativeJoin() throws DataAccessException {
-        throw new RuntimeException("negative join test not implemented");
+        UserService userService = new UserService(new MemoryUserDAO(), new MemoryAuthTokenDAO());
+        GameService gameService = new GameService(new MemoryGameDAO(), userService.getAuthTokenDAO(), userService.getUserDAO());
+
+        RegisterRequest newUser = new RegisterRequest("sally","123","sallyisawesome@gmail.com");
+        RegisterResult registeredUser = userService.register(newUser);
+        RegisterRequest newUser2 = new RegisterRequest("benson","321","benisawesome@gmail.com");
+        RegisterResult registeredUser2 = userService.register(newUser2);
+        RegisterRequest newUser3 = new RegisterRequest("jason","312","jasonisawesome@gmail.com");
+        RegisterResult registeredUser3 = userService.register(newUser3);
+
+        CreateRequest newGame = new CreateRequest("Black taken and full game", registeredUser.authToken());
+        CreateResult createdGame = gameService.create(newGame);
+
+        JoinRequest newPlayer = new JoinRequest("BLACK", createdGame.gameID(), registeredUser.authToken());
+        gameService.join(newPlayer);
+        JoinRequest newPlayer2 = new JoinRequest("BLACK", createdGame.gameID(), registeredUser2.authToken());
+        Assertions.assertThrows(DataAccessException.class, () -> gameService.join(newPlayer2),
+                "Player joined black when black was taken");
+        JoinRequest newPlayer2Retry = new JoinRequest("WHITE", createdGame.gameID(), registeredUser2.authToken());
+        gameService.join(newPlayer2Retry);
+        JoinRequest newPlayer3 = new JoinRequest("WHITE", createdGame.gameID(), registeredUser3.authToken());
+        Assertions.assertThrows(DataAccessException.class, () -> gameService.join(newPlayer3),
+                "Player joined full game");
+
+
+        CreateRequest newGame2 = new CreateRequest("White taken", registeredUser.authToken());
+        CreateResult createdGame2 = gameService.create(newGame2);
+
+        JoinRequest newPlayer5 = new JoinRequest("WHITE", createdGame2.gameID(), registeredUser.authToken());
+        gameService.join(newPlayer5);
+        JoinRequest newPlayer6 = new JoinRequest("WHITE", createdGame2.gameID(), registeredUser2.authToken());
+        Assertions.assertThrows(DataAccessException.class, () -> gameService.join(newPlayer6),
+                "Player joined white when white was taken");
+
+        JoinRequest newPlayer7 = new JoinRequest("BLACK", 2, registeredUser2.authToken());
+        Assertions.assertThrows(DataAccessException.class, () -> gameService.join(newPlayer7),
+                "Player joined non-existent game");
     }
 
     @Test
