94df3f2b1df7c2f9c98b016ab10c434adf1ac847
samuelSchulthies <samuelschulthies@gmail.com>
2024-10-23 22:54:31 -0600

    small readability changes

--- server/src/main/java/handler/ExceptionHandler.java
+++ server/src/main/java/handler/ExceptionHandler.java
@@ -8,11 +8,12 @@ import spark.Response;
 import java.util.Map;
 
 public class ExceptionHandler {
-    public Object exception(DataAccessException ex, Request req, Response res){
-        var body = new Gson().toJson(Map.of("message", String.format("Error: %s", ex.getMessage()), "success", false));
+    public void exception(DataAccessException ex, Request req, Response res){
+        var body = new Gson().toJson(Map.of(
+                "message", String.format("Error: %s", ex.getMessage()),
+                "success", false));
         res.type("application/json");
         res.status(ex.statusCode());
         res.body(body);
-        return body;
     }
 }
