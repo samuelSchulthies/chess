64bb285ef58c732af0fd50c657adc93024165849
samuelSchulthies <samuelschulthies@gmail.com>
2024-11-09 18:36:18 -0700

    First test done: positive register result works

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -1,17 +1,28 @@
 package client;
 
+import dataaccess.DataAccessException;
+import dataaccess.MySQLAuthTokenDAO;
+import dataaccess.MySQLGameDAO;
+import dataaccess.MySQLUserDAO;
 import org.junit.jupiter.api.*;
+import requestresult.*;
 import server.Server;
+import server.ServerFacade;
+import service.ClearService;
+import service.GameService;
+import service.UserService;
 
 
 public class ServerFacadeTests {
 
     private static Server server;
+    private static ServerFacade serverFacade;
 
     @BeforeAll
     public static void init() {
         server = new Server();
         var port = server.run(0);
+        serverFacade = new ServerFacade("http://localhost:" + port);
         System.out.println("Started test HTTP server on " + port);
     }
 
@@ -22,8 +33,88 @@ public class ServerFacadeTests {
 
 
     @Test
-    public void sampleTest() {
-        Assertions.assertTrue(true);
+    @DisplayName("Proper Register Result")
+    public void positiveRegister() throws DataAccessException {
+        UserService userService = new UserService(new MySQLUserDAO(), new MySQLAuthTokenDAO());
+        GameService gameService = new GameService(new MySQLGameDAO(), userService.getAuthTokenDAO());
+        ClearService clearService = new ClearService(userService, gameService);
+
+        clearService.clear();
+
+        RegisterRequest newUser = new RegisterRequest("sally","123","sallyisawesome@gmail.com");
+        RegisterResult registerResult = serverFacade.register(newUser);
+
+        Assertions.assertEquals(newUser.username(), registerResult.username(),
+                "Username of registered user does not match database");
+        Assertions.assertNotNull(registerResult.authToken(), "Registered user did not return authToken");
+
+        clearService.clear();
+    }
+
+    @Test
+    @DisplayName("Improper Register Result")
+    public void negativeRegister() throws DataAccessException {
+    }
+
+    @Test
+    @DisplayName("Proper Login Result")
+    public void positiveLogin() throws DataAccessException {
+    }
+
+    @Test
+    @DisplayName("Improper Login Result")
+    public void negativeLogin() throws DataAccessException {
+
+    }
+
+    @Test
+    @DisplayName("Proper Logout Result")
+    public void positiveLogout() throws DataAccessException {
+
+    }
+
+    @Test
+    @DisplayName("Improper Logout Result")
+    public void negativeLogout() throws DataAccessException {
+
+    }
+
+    @Test
+    @DisplayName("Positive Create Result")
+    public void positiveCreate() throws DataAccessException {
+    }
+
+    @Test
+    @DisplayName("Negative Create Result")
+    public void negativeCreate() throws DataAccessException {
+    }
+
+    @Test
+    @DisplayName("Positive Join Result")
+    public void positiveJoin() throws DataAccessException {
+    }
+
+    @Test
+    @DisplayName("Negative Join Result")
+    public void negativeJoin() throws DataAccessException {
+    }
+
+    @Test
+    @DisplayName("Positive List Result")
+    public void positiveList() throws DataAccessException {
+
+    }
+
+    @Test
+    @DisplayName("Negative List Result")
+    public void negativeList() throws DataAccessException {
+
+    }
+
+    @Test
+    @DisplayName("Clear Test")
+    public void clearTest() throws DataAccessException {
+
     }
 
 }
---
+++ server/src/main/java/server/ServerFacade.java
@@ -0,0 +1,103 @@
+package server;
+
+import com.google.gson.Gson;
+import dataaccess.DataAccessException;
+import requestresult.*;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URL;
+
+public class ServerFacade {
+    private final String serverUrl;
+
+    public ServerFacade(String serverUrl) {
+        this.serverUrl = serverUrl;
+    }
+
+    public void clear() throws DataAccessException {
+        makeRequest("DELETE", "/db", null, null, null);
+    }
+
+    public CreateResult create(CreateRequest createRequest, String authToken) throws DataAccessException {
+        return makeRequest("POST", "/game", createRequest, authToken, CreateResult.class);
+    }
+
+    public JoinResult join(JoinRequest joinRequest, String authToken) throws DataAccessException {
+        return makeRequest("PUT", "/game", joinRequest, authToken, JoinResult.class);
+    }
+
+    public ListResult list(String authToken) throws DataAccessException {
+        return makeRequest("GET", "/game", null, authToken, ListResult.class);
+    }
+
+    public LoginResult login(LoginRequest loginRequest) throws DataAccessException {
+        return makeRequest("POST", "/session", loginRequest, null, LoginResult.class);
+    }
+
+    public void logout(String authToken) throws DataAccessException {
+        makeRequest("DELETE", "/session", null, authToken, null);
+    }
+
+    public RegisterResult register(RegisterRequest registerRequest) throws DataAccessException {
+        return makeRequest("POST", "/user", registerRequest, null, RegisterResult.class);
+    }
+
+
+    private <T> T makeRequest(String method, String path, Object request, String authToken, Class<T> responseClass) throws DataAccessException{
+        try {
+            URL url = (new URI(serverUrl + path).toURL());
+            HttpURLConnection http = (HttpURLConnection) url.openConnection();
+            http.setRequestMethod(method);
+            http.setDoOutput(true);
+
+            if (authToken != null){
+                writeHeader(authToken, http);
+            }
+            writeBody(request, http);
+            http.connect();
+            throwIfError(http);
+            return readBody(http, responseClass);
+        } catch (Exception e){
+            throw new DataAccessException(e.getMessage());
+        }
+    }
+
+    private static void writeBody(Object request, HttpURLConnection http) throws IOException {
+        if (request != null){
+            http.addRequestProperty("Content-Type", "application/json");
+            String reqData = new Gson().toJson(request);
+            try (OutputStream reqBody = http.getOutputStream()){
+                reqBody.write(reqData.getBytes());
+            }
+        }
+    }
+
+    private static void writeHeader(String authToken, HttpURLConnection http){
+        http.addRequestProperty("Authorization", authToken);
+    }
+
+    private static <T> T readBody (HttpURLConnection http, Class<T> responseClass) throws IOException{
+        T response = null;
+        if (http.getContentLength() < 0){
+            try (InputStream resBody= http.getInputStream()){
+                InputStreamReader reader = new InputStreamReader(resBody);
+                if(responseClass != null){
+                    response = new Gson().fromJson(reader, responseClass);
+                }
+            }
+        }
+        return response;
+    }
+
+    private void throwIfError(HttpURLConnection http) throws IOException, DataAccessException {
+        if (http.getResponseCode() != 200){
+            throw new DataAccessException("An error occurred");
+        }
+    }
+
+}
