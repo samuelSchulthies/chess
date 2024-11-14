8d9f55ac5c13f763953aaae980e34595da639f0e
samuelSchulthies <samuelschulthies@gmail.com>
2024-11-14 16:45:47 -0700

    chessboard nearly done

--- client/src/main/java/client/ChessBoardUI.java
+++ client/src/main/java/client/ChessBoardUI.java
@@ -12,8 +12,9 @@ public class ChessBoardUI {
 
     ChessBoard board = new ChessBoard();
 
-    private static final String rowLabels = "12345678";
-    private static final String columnLabels = "   a  b  c  d  e  f  g  h   ";
+    private static final String ROW_LABELS = "12345678";
+    private static final String COLUMN_LABELS = "    h  g  f  e  d  c  b  a    ";
+    private static final String SQUARE = "   ";
 
     public ChessBoardUI(){
         board.resetBoard();
@@ -25,30 +26,58 @@ public class ChessBoardUI {
         out.print(ERASE_SCREEN);
 
         labelColumns(out);
+        clearBoarders(out);
         out.println();
         labelRows(out);
         labelColumns(out);
     }
 
     public static void labelColumns(PrintStream out){
-        setBoarders(out);
+        setBoardersGrey(out);
 
-        for(char col : columnLabels.toCharArray()){
+        for(char col : COLUMN_LABELS.toCharArray()){
             out.print(col);
         }
     }
     public static void labelRows(PrintStream out){
-        setBoarders(out);
 
-        for(char row : rowLabels.toCharArray()){
+        for(int row = 0; row < ROW_LABELS.length(); ++row){
+            setBoardersGrey(out);
             out.print(" ");
-            out.println(row);
+            out.print(ROW_LABELS.charAt(row));
+            out.print(" ");
+            setSquares(out, row);
+            clearBoarders(out);
+            out.print("\n");
         }
     }
 
-    public static void setBoarders(PrintStream out){
+    public static void setSquares(PrintStream out, int row){
+        int colorSwitcher = 0;
+        for (int col = 0; col < 9; ++col){
+            if (row % 2 == 1){
+                colorSwitcher = 1;
+            }
+            if ((col % 2 == colorSwitcher) && (col != 8)) {
+                out.print(SET_BG_COLOR_WHITE);
+            }
+            else if (col == 8){
+                setBoardersGrey(out);
+            }
+            else {
+                out.print(SET_BG_COLOR_BLACK);
+            }
+            out.print(SQUARE);
+        }
+    }
+
+    public static void setBoardersGrey(PrintStream out){
         out.print(SET_BG_COLOR_LIGHT_GREY);
         out.print(SET_TEXT_COLOR_BLACK);
+    }
+
+    public static void clearBoarders(PrintStream out){
+        out.print(RESET_BG_COLOR);
     }
 
 
